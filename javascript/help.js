f4cd7e732a423e1341fec7268ed3e4f8e727498e
illhwa_drago <129027761+illhwa@users.noreply.github.com>
2024-05-28 15:23:58 +0900

    사이드바 기능 추가

--- javascript/help.js
+++ javascript/help.js
@@ -1,3 +1,10 @@
+function updateActiveLink(newActiveLink) {
+    document.querySelectorAll('.help-link').forEach(link => {
+        link.classList.remove('active');
+    });
+    newActiveLink.classList.add('active');
+}
+
 function unknownfunction() {
     document.querySelectorAll('.help-link').forEach(link => {
         link.addEventListener('click', function(event) {
@@ -6,6 +13,7 @@ function unknownfunction() {
                 section.style.display = 'none';
             });
             document.getElementById(this.dataset.content).style.display = 'block';
+            updateActiveLink(this);
         });
     });
 }
@@ -13,7 +21,7 @@ function unknownfunction() {
 function changeTheme() {
     const header = document.querySelector('.header');
     const footer = document.querySelector('.footer');
-    const themeColor = localStorage.getItem("theme")
+    const themeColor = localStorage.getItem("theme");
     header.style.backgroundColor = themeColor;
     footer.style.backgroundColor = themeColor;
 }
@@ -21,4 +29,9 @@ function changeTheme() {
 document.addEventListener('DOMContentLoaded', () => {
     unknownfunction();
     changeTheme();
+    const firstLink = document.querySelector('.help-link');
+    if (firstLink) {
+        firstLink.classList.add('active');
+        document.getElementById(firstLink.dataset.content).style.display = 'block';
+    }
 });
